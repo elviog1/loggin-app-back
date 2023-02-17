4509f3d4d7707636d561783e031668672b2fed83
Elvio Galeano <elvio.galeano827@gmail.com>
2023-02-17 20:24:51 -0300

    sendMail function

---
+++ config/sendMail.js
@@ -0,0 +1,36 @@
+const nodemailer = require('nodemailer')
+const sendMail = async(email,code) =>{
+    const transport = nodemailer.createTransport({
+        service: 'gmail',
+            auth: {
+                user: process.env.EMAIL,
+                pass:process.env.PASS
+            },
+            tls: {
+                rejectUnauthorized: false
+            }
+    })
+    const mailOptions = {
+        from: process.env.EMAIL,
+        to: email,
+        subject: "title",
+        html: `
+        <main>
+            <div>
+                <h1>Hello, to finish with the registration you just have to enter the following link. Welcome to Login-app !
+                </h1>
+                <a href='http://localhost:4000/auth/verify/${code}'>Click here</a>
+            </div>
+        </main>
+        `
+    }
+    await transport.sendMail(mailOptions,(error,response)=>{
+        if(error){
+            console.log(error)
+        }else{
+            console.log(response)
+        }
+    })
+}
+
+module.exports =sendMail

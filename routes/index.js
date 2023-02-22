0cba874d49f2d43069822370dcb439a90ee43a4b
Elvio Galeano <elvio.galeano827@gmail.com>
2023-02-22 01:31:15 -0300

    add router.use comments

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
 const userRouter = require('./auth')
+const commentRouter = require('./comment')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.json({title:"hola"});
 });
 
 router.use('/auth',userRouter)
+router.use('/comment',commentRouter)
 
 module.exports = router; 

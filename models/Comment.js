cb71bfc2cb0b4c8eaf29f45e5b34eb667953ad55
Elvio Galeano <elvio.galeano827@gmail.com>
2023-02-24 00:42:58 -0300

    add date key

--- models/Comment.js
+++ models/Comment.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 
 const commentSchema = new mongoose.Schema({
     user: {type: mongoose.Types.ObjectId, ref:'users', required:true },
-    comment: {type: String, required:true}
+    comment: {type: String, required:true},
+    date: {type: String, required:true},
 })
 
 const Comment = mongoose.model('comments', commentSchema)

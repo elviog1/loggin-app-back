b9a765d5b8392e81cd9419f4166bba6b270f4552
Elvio Galeano <elvio.galeano827@gmail.com>
2023-02-24 16:33:49 -0300

    add delete router

--- routes/comment.js
+++ routes/comment.js
@@ -2,9 +2,10 @@ let express = require('express')
 let router = express.Router()
 const passport = require('passport')
 
-const {createComment, getAllComment} = require('../controllers/commentControllers')
+const {createComment, getAllComment, deleteComment} = require('../controllers/commentControllers')
 
 router.get('/',getAllComment)
 router.post('/',createComment)
+router.delete('/:id',deleteComment)
 
 module.exports = router

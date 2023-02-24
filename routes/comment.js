53f9ff8fa9ccd8330f1c408287759e1cd3aa64d7
Elvio Galeano <elvio.galeano827@gmail.com>
2023-02-24 18:11:28 -0300

    remove passport

--- routes/comment.js
+++ routes/comment.js
@@ -1,6 +1,5 @@
 let express = require('express')
 let router = express.Router()
-const passport = require('passport')
 
 const {createComment, getAllComment, deleteComment} = require('../controllers/commentControllers')
 

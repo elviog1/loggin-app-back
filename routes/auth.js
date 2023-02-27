fe2de1b93442d57a21e0f284b1f97f1254ccf90f
Elvio Galeano <elvio.galeano827@gmail.com>
2023-02-27 19:57:35 -0300

    new routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,15 @@
 let express = require('express')
 let router = express.Router()
-
-const {signUp, getAllUser, signIn, verifyMail, signOut} = require('../controllers/userController')
+// let passport = require('../config/passport')
+const {signUp, getAllUser, signIn, verifyMail, signOut,verifyToken, update, getUser} = require('../controllers/userController')
 
 router.get('/',getAllUser)
+router.get('/:id',getUser)
 router.post('/signup',signUp)
 router.post('/signin',signIn)
 router.post('/signout/:email',signOut)
 router.get('/verify/:code',verifyMail)
+router.put('/:id',update)
+// router.get('/token', passport.authenticate('jwt',{session:false}), verifyToken)
 
 module.exports = router
--- routes/comment.js
+++ routes/comment.js
@@ -1,5 +1,6 @@
 let express = require('express')
 let router = express.Router()
+// let passport = require('../config/passport')
 
 const {createComment, getAllComment, deleteComment} = require('../controllers/commentControllers')
 
@@ -7,4 +8,5 @@ router.get('/',getAllComment)
 router.post('/',createComment)
 router.delete('/:id',deleteComment)
 
+// router.post('/',passport.authenticate('jwt',{session:false}),createComment)
 module.exports = router
